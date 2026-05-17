777c2f2ee3871c00bb51ba9576d5f5f068eb6f9c
Cardoso8586 <jucelino.7785mendes@gmail.com>
2026-05-17 14:13:55 -0300

    primeiro commit

--- src/main/java/com/boss_battle/controller/ZerAdsBossController.java
+++ src/main/java/com/boss_battle/controller/ZerAdsBossController.java
@@ -28,15 +28,21 @@ public class ZerAdsBossController {
             @RequestParam(defaultValue = "0") Integer clicks,
             HttpServletRequest request
     ) {
-        String ip = request.getRemoteAddr();
+        System.out.println("===== CALLBACK ZERADS =====");
+        System.out.println("PWD: " + pwd);
+        System.out.println("USER: " + user);
+        System.out.println("AMOUNT: " + amount);
+        System.out.println("CLICKS: " + clicks);
+        System.out.println("IP: " + request.getRemoteAddr());
 
         if (!PASSWORD.equals(pwd)) {
             return ResponseEntity.status(403).body("Senha inválida");
         }
 
-        if (!ZERADS_IP.equals(ip)) {
-            return ResponseEntity.status(403).body("IP inválido: " + ip);
-        }
+        // deixe comentado só para teste
+        // if (!ZERADS_IP.equals(request.getRemoteAddr())) {
+        //     return ResponseEntity.status(403).body("IP inválido: " + request.getRemoteAddr());
+        // }
 
         BigDecimal recompensa = zerAdsBossService.creditarRecompensa(user, amount, clicks);
 
